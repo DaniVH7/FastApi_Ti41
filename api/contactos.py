c71630ea1b4c9eb66e874eef41344dfcdc6bf64e
Cristian-9 <1721110069@utectulancingo.edu.mx>
2022-10-03 12:49:22 +0000

    Updated crear nu

--- api/contactos.py
+++ api/contactos.py
@@ -1,32 +1,72 @@
 from fastapi import FastAPI
 import sqlite3
-from typing import list
+from typing import List
 from pydantic import BaseModel
-from pydantic import Emailstr
 from fastapi import HTTPException 
 from fastapi import status
 
-app = fastapi(tittle="API para registros", description="Nothing")
+
+class Mensaje(BaseModel):
+    mensaje:str
+
+class Contacto(BaseModel):
+    id_contacto: int
+    nombre: str
+    email: str
+    telefono: str
+
+description = """
+# Contactos API Rest
+Implementacion de una api rest con conexiona base de datos para realizar crud 
+"""
+
+app = FastAPI(
+    title="API para registros", 
+    description="description",
+    version= "0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Cristian VH",
+        "url":"http://github.com/DaniVH7",
+        "email":"1721110069@utectulancingo.edu.mx",
+    },
+    license_info={
+        "name":"Apache 2.0",
+        "url":"http://www.apache.org/licenses/LICENSE-2.0.html",
+    })
+
 # 1
-@appget(
-    "/Contactos/",
-    responsemodel = list[Contactos],
-    statuscode = status.HTTP_202_ACCEPTED,
+@app.get(
+    "/",
+    response_model=Mensaje,
+    status_code=status.HTTP_202_ACCEPTED,
+    summary="Enpoint principal",
+    description="Regresa un mensaje",
+)
+async def read_root():
+    response={"mensaje":"Holisss"}
+    return response
+
+#2
+@app.get(
+    "/contactos/",
+    response_model = List[Contacto],
+    status_code = status.HTTP_202_ACCEPTED,
     summary ="Lista de Contactos",
     description = "Endpoint que regresa un array con todos los Contactos"
 )
+
 async def get_contactos():
     try:
-        with sqlite3.connect("api/sql/contactos.sql") as connection:
-            connection.row.factory = sqlite3.Row
+        with sqlite3.connect("sql/contactos.db") as connection:
+            connection.row_factory = sqlite3.Row
             cursor = connection.cursor()
-            cursor.execute("Select id_contacto, nombre, email, telefono from contactos;")
+            cursor.execute("SELECT id_contacto, nombre, email, telefono FROM contactos;")
             response = cursor.fetchall()
             return response
     except Exception as error:
         print(f"ERROR en get_contactos{error.args}")
-        raise HTTPSException(
-            statuscode = status.HTTP_400_BAD_REQUEST,
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
             detail="Error al consultar los datos"
         )
-
--- api/sql/contactos.sql
+++ api/sql/contactos.sql
@@ -1,8 +1,8 @@
-Create database contactos
-use contactos;
-Create table  if not exists contactos(
-    id_contacto int primary key Autoincrement,
+Create table  contactos(
+    id_contacto integer primary key Autoincrement,
     nombre varchar(250),
     email varchar(250),
     telefono varchar(10)
 );
+insert into contactos values(null,"Cristian","cristian@gmail.com","7766554433");
+insert into contactos values(null,"Daniel","Daniel@gmail.com","7766998877");
