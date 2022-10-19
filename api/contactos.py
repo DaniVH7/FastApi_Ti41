fa059a7896bfb40c797f3765678b4b786ed68ed3
Cristian-9 <1721110069@utectulancingo.edu.mx>
2022-10-19 15:51:17 +0000

    Agrego la consulta para eliminar un Conntacto

--- api/contactos.py
+++ api/contactos.py
@@ -136,3 +136,38 @@ async def post_contactos(contacto:Contacto):
             status_code = status.HTTP_400_BAD_REQUEST,
             detail=" Error Fatal 💀 "
         )
+
+#4 Update
+@app.put(
+    "/contactos/{id_contacto}",
+    response_model = Mensaje, 
+    status_code = status.HTTP_202_ACCEPTED,
+    summary ="Actualizar contacto",
+    description = "Endpoint para actualizar un contacto"
+)
+async def put_contactos(id_contacto:int, contacto:Contacto):
+    try:
+        with sqlite3.connect("api/sql/contactos.db") as connection:
+            connection.row_factory = sqlite3.Row
+            cursor = connection.cursor()
+            consult = "SELECT email from contactos;"
+            cursor.execute(consult)
+            result = cursor.fetchall()
+            existent_email = []
+            for row in result:
+                    existent_email.append(str(row['email']))
+            if contacto.email in existent_email:
+                    response = {"mensaje":"Correo en uso, intenta ingresar uno nuevo"}
+                    return response
+            else:
+                sql="UPDATE contactos SET nombre=?, email =? ,telefono= ? WHERE id_contacto= ?;"
+                values=(contacto.nombre, contacto.email,contacto.telefono, id_contacto, )
+                cursor.execute(sql, values)
+                response = {"mensaje":"Contacto Actualizado"}
+                return response
+    except Exception as error:
+        print(f"Error al consultar tus datos{error.args}")
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail=" Error Fatal 💀 "
+        )
